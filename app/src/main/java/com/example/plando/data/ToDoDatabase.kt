c1a2ed4645347103545d344c1e714eb7fc6d424d
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2022-02-13 16:54:44 +0100

    Setup Dagger-hilt for DI

--- app/src/main/java/com/example/plando/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/plando/data/ToDoDatabase.kt
@@ -1,9 +1,10 @@
 package com.example.plando.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.plando.data.models.ToDoTask
 
 @Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
-abstract class ToDoDatabase {
+abstract class ToDoDatabase : RoomDatabase() {
     abstract fun toDoDao(): ToDoDao
 }
