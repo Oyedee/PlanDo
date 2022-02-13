953908328cf5d6f2f62acbe526cd000ab32ce934
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2022-02-13 17:22:34 +0100

    setting up the repository and viewModel for our todo app

---
+++ app/src/main/java/com/example/plando/ui/viewmodel/SharedViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.plando.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.plando.data.models.ToDoTask
+import com.example.plando.data.repository.ToDoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SharedViewModel @Inject constructor(
+    private val toDoRepository: ToDoRepository
+) : ViewModel() {
+
+    private val _allTasks =
+        MutableStateFlow<List<ToDoTask>>(emptyList())
+
+    val allTasks: StateFlow<List<ToDoTask>> = _allTasks
+
+    fun getAllTasks() {
+        viewModelScope.launch {
+            toDoRepository.getAllTasks.collect {
+                _allTasks.value = it
+            }
+        }
+    }
+}
